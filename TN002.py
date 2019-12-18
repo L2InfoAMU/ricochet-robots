d0221a08942f386a906ac5d1ff7e7c7fba4a085a
Canals <MCanals@ac-nancy-metz.fr>
2019-12-18 15:01:02 +0100

    Create TN002.py

---
+++ TN002.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 22:48:38 2019
+Exemple de la note technique
+@author: Martin
+"""
+
+import PySimpleGUI as sg
+game_zone=sg.Graph(canvas_size=(400,400) ,\
+                   graph_bottom_left = (-1,-1),\
+                   graph_top_right=(11,11),\
+                   background_color='White',\
+                   enable_events = True,\
+                   key='graph',\
+                   float_values=False)    
+
+layout = [[game_zone]]
+window = sg.Window('grid', layout,return_keyboard_events=True
+                   ) 
+window.Finalize()  
+
+#drawing the grid
+for i in range(11) :
+    game_zone.DrawLine((0,i), (10,i),width=1,color='grey') 
+    game_zone.DrawLine((i,0), (i,10),width=1,color='grey') 
+
+while True:                             # The Event Loop
+    event, values = window.read() 
+#   print(event)
+    print(values) 
