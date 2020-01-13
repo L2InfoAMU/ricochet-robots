0b7df78060ffeb68d17b902a69862eb2654a1665
Canals <MCanals@ac-nancy-metz.fr>
2020-01-13 21:16:57 +0100

    Create RRCONST.py
    Fichiers des constantes du projet

---
+++ RRCONST.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 11:32:57 2020
+
+@author: Martin
+"""
+
+# constantes pour le projet
+
+MAX_ROBOTS = 5
+
+BLUE = 0
+GREEN = 1
+RED = 2
+YELLOW = 3
+BLACK = 4
+
+
+# pour les directions de mouvement
+
+SOUTH       = 0b0001
+EAST        = 0b0010
+NORTH       = 0b0100
+WEST        = 0b1000
