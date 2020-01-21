3dbb6cd7d20403bfc010cf468ce9e547ce776572
Canals <MCanals@ac-nancy-metz.fr>
2020-01-21 23:17:29 +0100

    Update tester_game.py

--- tester_game.py
+++ tester_game.py
@@ -8,9 +8,17 @@ Created on Tue Dec 10 03:04:54 2019
 
 #• test de la classe game
 
-import game 
+import grid
+import goal
+import robot
+import game
 
-test_grid =[[9, 8, 8, 8, 10, 12],
+SOUTH = grid.SOUTH
+EAST = grid.EAST
+NORTH = grid.NORTH    
+WEST = grid.WEST
+
+table =[[9, 8, 8, 8, 10, 12],
             [1, 0, 2, 4,  9, 6],
             [3, 0, 8, 0,  0, 12],
             [9, 0, 0, 0,  0, 4],
@@ -18,36 +26,18 @@ test_grid =[[9, 8, 8, 8, 10, 12],
             [7, 3, 2, 2, 10, 6]]
 
 
-"""
- - - - - - -
-| |
-"""
+test_grid = grid.Grid(table)
+r0 = robot.Robot((3,0),0)
+r1 = robot.Robot((4,4),1)
+robots=[r0,r1]
+goal = goal.Goal(r0 ,(1,1))
 
-r1 = game.Robot((3,0),1)
-r2 = game.Robot((4,4),2)
-jeutest=game.Game(test_grid,robots=[r1,r2])
+jeutest=game.Game(test_grid,robots,goal)
 
+print(robots)
+r0.move(NORTH)
 
+print(robots)
+r1.move(SOUTH)
 print(jeutest.robots)
-jeutest.move_robot(r1,game.NORTH)
 
-print(jeutest.robots)
-jeutest.move_robot(r2,game.SOUTH)
-print(jeutest.robots)
-jeutest.move_robot(r2,game.EAST)
-print(jeutest.robots)
-jeutest.move_robot(r2,game.NORTH)
-print(jeutest.robots)
-jeutest.move_robot(r2,game.WEST)
-
-print(jeutest.robots)
-jeutest.move_robot(r2,game.SOUTH)
-print(jeutest.robots)
-jeutest.move_robot(r2,game.EAST)
-print(jeutest.robots)
-jeutest.move_robot(r2,game.NORTH)
-print(jeutest.robots)
-jeutest.move_robot(r1,game.SOUTH)
-print(jeutest.robots)
-jeutest.move_robot(r1,game.EAST)
-print(jeutest.robots)
