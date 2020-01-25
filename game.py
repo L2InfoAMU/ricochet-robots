75653e826c090f7a78050148d378dd07d3675e45
Canals <MCanals@ac-nancy-metz.fr>
2020-01-25 16:25:41 +0100

    Update game.py
    Renommage de la classe en GameWorld
    Ajout de l'attribut goal
    Ajout du lien dans les instances de la classe Robot

--- game.py
+++ game.py
@@ -6,23 +6,23 @@ import robot
 import goal    
 import board
 
-class Game :
+class GameWorld :
     """  la classe jeu regroupe les données décrivant 
     - le plateau de jeu 
     - la liste des robots
     - l'objectif du jeu
     """    
-    def __init__(self, board, robots) :
+    def __init__(self, board , robots , goal) :
         """ renvoie un objet de type Game
         """        
         self.board = board        
         self.robots = robots
+        self.goal = goal
 
         self.nb_robots = len(robots)
         
-        for robot in robots :
-            robot.board = board
-            robot.friends = robots
+        for robot in robots : robot.world = self
+            
                 
 
        
