90f69f15673395b34616334aa5249da5512eb608
binfrancois <58139017+binfrancois@users.noreply.github.com>
2020-03-06 23:22:07 +0100

    nettoyage +  ajout menu
    ne marche pas encore ! Comment on fait un repaint() pour changer de grille si on appelle la random_classic_board ?

--- test_PySide.py
+++ test_PySide.py
@@ -2,19 +2,26 @@ import sys
 from PySide2.QtWidgets import QApplication, QWidget, QMainWindow, QGridLayout, QLabel, QPushButton, QMainWindow, QAction
 from PySide2.QtGui import QKeySequence, QPainter, QColor, QBrush, QPaintEvent, QFont, QPen
 from PySide2.QtCore import Qt
+from random import *
+from ClassicBoard import *
+from objectif import Objectif
+from RRCONST import *
 
 import game as g
 
-test_grid = [[9, 8, 8, 8, 10, 12],
-             [1, 0, 2, 4, 9, 6],
-             [3, 0, 8, 0, 0, 12],
-             [9, 0, 0, 0, 0, 4],
-             [1, 4, 1, 0, 6, 5],
-             [7, 3, 2, 2, 10, 6]]
+test_grid = [[ 9,  1, 1, 1,  3, 9, 1, 1],
+              [  8,   0, 6, 8, 0,  0, 0, 0],
+              [  8,  0, 1, 0, 0,  0, 0, 0],
+              [  10, 12, 0, 0, 0,  0, 4, 0],
+              [ 12, 1, 0, 0, 0,  2, 9, 0],
+              [ 9, 0, 0, 0, 0,  4, 0, 0],
+              [ 8, 0, 0, 0, 0, 3, 8, 4],
+              [  8, 0, 0, 6, 8, 0, 2, 9]]
 
+test_board = ClassicBoard(test_grid, Objectif(RED,(1,1)))
 
 class MaFenetre(QMainWindow):
-    GAMEZONE_SIZE = 300
+    GAMEZONE_SIZE = 400
 
     def __init__(self):
         super().__init__()
@@ -24,9 +31,11 @@ class MaFenetre(QMainWindow):
         self.menu = self.menuBar()
         self.file_menu = self.menu.addMenu("File")
         self.options_menu = self.menu.addMenu("Options")
+        self.size_fenetre = self.geometry()
+        GAMEZONE_SIZE = min(self.size_fenetre.width(), self.size_fenetre.height()) / 2
 
         # New Qaction
-        new_action = QAction("New", self)
+        new_action = QAction("New", self) # à compléter
         new_action.setShortcut(QKeySequence.New)
 
         # Exit QAction
@@ -38,14 +47,41 @@ class MaFenetre(QMainWindow):
         self.file_menu.addAction(exit_action)
 
         # Options QAction
-        options_action = QAction("Op")
+        options_action1 = QAction("Une grille classique au hasard", self)
+        self.options_menu.addAction(options_action1)
+        options_action1.triggered.connect(self.level_choice())  # à modifier/compléter
+
+
+        options_action2 = QAction("Une grille aléatoire", self)
+        self.options_menu.addAction(options_action2)
+        #à compléter
+
+        options_action3 = QAction("choix d'une grille enregistrée", self)
+        self.options_menu.addAction(options_action3)
 
         # Status Bar
         self.status = self.statusBar()
         self.status.showMessage("Sur un jeu d'Alex Randolph")
 
-        widget = GameDesign(test_grid)
-        widget.resize(self.GAMEZONE_SIZE, self.GAMEZONE_SIZE)
+        layout = QGridLayout()
+        self.label = QLabel("Robot Ricochet", self)
+        self.button = QPushButton("Cliquez ici pour commencer", self)
+        # self.button.setFont(QFont("Verdana", 25))
+        widget2 = GameDesign(test_grid)
+        widget2.resize(self.GAMEZONE_SIZE, self.GAMEZONE_SIZE)
+
+        layout.addWidget(self.label, 0, 0, 1, 1)
+        layout.addWidget(widget2, 1, 1, 4, 4)
+        layout.addWidget(self.button, 0, 1, 1, 1)
+
+        self.button.setStyleSheet("color: blue; background-color: black")
+        self.label.setStyleSheet("color: blue; background-color: black")
+
+        # Add button signal to level_choice slot
+        self.button.clicked.connect(self.level_choice)
+        widget = QWidget()
+        widget.setLayout(layout)
+
         self.setCentralWidget(widget)
         self.creation_interface()
         self.show()
@@ -53,9 +89,12 @@ class MaFenetre(QMainWindow):
     def creation_interface(self):
         self.show()
 
+
+
+
     def level_choice(self):
         """
-        Affiche la page du menu de choix du niveau de difficulté """
+        Affiche la page du menu de choix du niveau de difficulté """   #à faire/modifier
         self.close()
 
 
@@ -102,11 +141,11 @@ class GameDesign(QWidget):
     def draw_walls(self, p):
         for i in range(self.grid_size_x):
             for j in range(self.grid_size_y):
-                cell = self.grid[i][j]
+                cell = self.grid[j][i]
                 if cell & g.SOUTH:
-                    self._draw_horizontal_wall_(p, (i, j))
+                    self._draw_horizontal_wall_(p, (i, j+1))
                 if cell & g.NORTH:
-                    self._draw_horizontal_wall_(p, (i, j + 1))
+                    self._draw_horizontal_wall_(p, (i, j))
                 if cell & g.EAST:
                     self._draw_vertical_wall_(p, (i + 1, j))
                 if cell & g.WEST:
@@ -115,19 +154,19 @@ class GameDesign(QWidget):
     def draw_robot(self):
         # On ajoute un bouton dans la zone de dessin
         robot = QPushButton("", self)
-        robot.setGeometry(20, 20, 70, 70)
+        robot.setGeometry(10, 10, 35, 35)
         # On choisit une image à mettre sur le bouton
-        robot.setStyleSheet("background-image: url(robot rouge.png);");
+    #    robot.setStyleSheet("background-image: url(robot rouge.png);");
 
     def paintEvent(self, e):
         p = QPainter(self)
 
-        brush = QBrush(QColor(255, 0, 0))
+        brush = QBrush(QColor(0, 0, 0))
         p.setBrush(brush)
 
         self.draw_grid(p)
         wall_pen = QPen()
-        wall_pen.setColor(QColor(255, 0, 0))
+        wall_pen.setColor(QColor(0, 0, 0))
         wall_pen.setWidth(3)
         p.setPen(wall_pen)
 
