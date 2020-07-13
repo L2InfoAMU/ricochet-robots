00c0ec51cc01892b96a53f37e70b4b6f9ef7414a
binfrancois <58139017+binfrancois@users.noreply.github.com>
2020-07-13 15:41:42 +0200

    test avec vs

--- 2eme version/gamedesign.py
+++ 2eme version/gamedesign.py
@@ -1,30 +1,32 @@
 """ La classe GameDesign est responsable de dessiner le plateau de jeu avec les robots
-
+ceci est une modification de test
 """
 
 import sys
-from PySide2.QtWidgets import QApplication, QWidget, QMainWindow, QGridLayout, QLayout
-from PySide2.QtGui import QImage
+from PySide2.QtCore import QSize, Qt, QTimer
+from PySide2.QtWidgets import QApplication, QWidget, QMainWindow, QGridLayout, QLayout, QLabel
+from PySide2.QtGui import QImage, QPixmap, QPainter
 from robot import *
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import Qt
 
 
-
-PIXMAPS = [ QImage("./icons/0.bmp"),
-            QImage("./icons/N.bmp"),
-            QImage("./icons/E.bmp"),
-            QImage("./icons/NE.bmp"),
-            QImage("./icons/S.bmp"),
-            QImage("./icons/NS.bmp"),
-            QImage("./icons/ES.bmp"),
-            QImage("./icons/NES.bmp"),
-            QImage("./icons/W.bmp"),
-            QImage("./icons/NW.bmp"),
-            QImage("./icons/EW.bmp"),
-            QImage("./icons/NEW.bmp"),
-            QImage("./icons/SW.bmp"),
-            QImage("./icons/NSW.bmp"),
-            QImage("./icons/ESW.bmp"),
-            QImage("./icons/NESW.bmp")]
+PIXMAPS = [ QImage("D:/icons/0.bmp"),
+            QImage("D:/icons/N.bmp"),
+            QImage("D:/icons/E.bmp"),
+            QImage("D:/icons/NE.bmp"),
+            QImage("D:/icons/S.bmp"),
+            QImage("D:/icons/NS.bmp"),
+            QImage("D:/icons/ES.bmp"),
+            QImage("D:/icons/NES.bmp"),
+            QImage("D:/icons/W.bmp"),
+            QImage("D:/icons/NW.bmp"),
+            QImage("D:/icons/EW.bmp"),
+            QImage("D:/icons/NEW.bmp"),
+            QImage("D:/icons/SW.bmp"),
+            QImage("D:/icons/NSW.bmp"),
+            QImage("D:/icons/ESW.bmp"),
+            QImage("D:/icons/NESW.bmp")]
 
 
 class Pos(QWidget):
@@ -37,7 +39,7 @@ class Pos(QWidget):
         self.setFixedSize(QSize(20, 20))
         self.x = x
         self.y = y
-        self.reset()
+
 
 
 
@@ -47,8 +49,29 @@ class Pos(QWidget):
 
         r = event.rect()   #dessine juste dans le Qwidget
         p.drawPixmap(r, QPixmap(PIXMAPS[str(self.game.board.grid[x][y])]))
+        painter.end()
+
+class WidgetTest(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel()
+        canvas = QPixmap(400, 300)
+        canvas.fill(Qt.white)
+        self.label.setPixmap(canvas)
+
+        self.draw_something()
 
 
+    def draw_something(self):
+        painter = QPainter(self.label.pixmap())
+
+        names=["Empty","N","E","EN","S","NS","ES","ENS","W","NW","EW","ENW","SW","NSW","ESW","ENSW"]
+        images = [QtGui.QPixmap("D:/images/"+name+".bmp", format="bmp")  for name in names]
+        painter.drawPixmap(QtCore.QPoint(0,0) , images[9])
+        painter.drawPixmap(QtCore.QPoint(50,0) , images[3])
+        painter.drawPixmap(QtCore.QPoint(0,50) , images[13])
+        painter.drawPixmap(QtCore.QPoint(50,50) , images[6])
+        painter.end()
 
 class MainWindow(QMainWindow):
 
@@ -60,8 +83,14 @@ class MainWindow(QMainWindow):
         # tag::grid[]
         self.grid = QGridLayout()
         self.grid.setSpacing(0)
-        self.grid.setSizeConstraint(QLayout.SetFixedSize)
+        #self.grid.setSizeConstraint(QLayout.SetFixedSize)
         # end::grid[]
+        a = WidgetTest()
+        self.grid.addWidget(a, 0, 0)
+        #self.init_map()
+        widget = QWidget()
+        widget.setLayout(self.grid)
+        self.setCentralWidget(widget)
 
 
 
@@ -82,13 +111,13 @@ class MainWindow(QMainWindow):
 
 app = QApplication(sys.argv)
 
-fd = open("D:/test2.txt",'r')
+fd = open("D:/test.txt",'r')
 A = Board.load_from_file(fd)
 group = Robot_group()
 r1 = Robot (group, RColors.RED, (0,0) )
-r2 = Robot (group, RColors.GREEN, (5,0) )
+r2 = Robot (group, RColors.GREEN, (1,0) )
 game = Game(A,group,None)
 window = MainWindow(game)
 window.show()
-
+print(game.get_state())
 app.exec_()
